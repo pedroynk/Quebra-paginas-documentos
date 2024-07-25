6ebe06a0f9e41426a59d332bacf56bab0db4c513
pedroynk <pedrolucas.rt@gmail.com>
2024-07-25 16:56:46 -0300

    altera README

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Script Separador de PDF
 
-Este script Python divide um documento PDF em novas páginas sempre que encontra um termo específico. No exemplo fornecido, ele divide o PDF ao encontrar o termo "Brasília - DF, 26 de julho de 2024" e insere o texto alinhado à esquerda nas novas páginas.
+Este script Python divide um documento PDF em novas páginas sempre que encontra um termo específico.
 
 ## Requisitos
 
--- SeparadorPDF.py
+++ SeparadorPDF.py
@@ -14,18 +14,18 @@ def split_pdf_on_term(input_pdf_path, output_pdf_path, split_term):
                 note = split_term + note
             new_page = output_pdf.new_page(width=page.rect.width, height=page.rect.height)
             text_lines = note.split('\n')
-            cursor_y = 50  
+            cursor_y = 50
             for line in text_lines:
                 new_page.insert_text((50, cursor_y), line, fontsize=11)
-                cursor_y += 15  
+                cursor_y += 15
     
     output_pdf.save(output_pdf_path)
     output_pdf.close()
 
-input_pdf_path = ## Caminho do PDF que deseja separar as páginas
-output_pdf_path = ## Caminho de saída do PDF já separado
+input_pdf_path = ## Caminho do PDF que deseja separar as páginas.
+output_pdf_path = ## Caminho de saída do PDF já separado.
 
-split_term = # Inserir termo a ser localizado para fazer a quebra
+split_term = # Inserir termo a ser localizado para fazer a quebra.
 
 split_pdf_on_term(input_pdf_path, output_pdf_path, split_term)
 
